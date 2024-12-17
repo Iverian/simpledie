0679d96e7c95a4ee09545a6c1ad246b08c12c065
Артем Старченко <41ways1ucky@gmail.com>
2024-12-18 02:23:09 +0300

    fix: add d10 to defs

--- src/defs.rs
+++ src/defs.rs
@@ -7,6 +7,7 @@ pub static D3: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(3));
 pub static D4: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(4));
 pub static D6: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(6));
 pub static D8: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(8));
+pub static D10: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(10));
 pub static D12: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(12));
 pub static D20: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(20));
 pub static D100: Lazy<Die<u32>> = Lazy::new(|| Die::uniform(100));
