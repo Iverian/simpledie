d376b5bcd5165ca1b1bbd99d73afff2f1d466338
Старченко Артем Владимирович <a.v.starchenko@tinkoff.ru>
2024-12-18 18:57:49 +0300

    fix

--- src/die.rs
+++ src/die.rs
@@ -2,6 +2,7 @@ use std::borrow::Borrow;
 use std::collections::btree_map::Entry;
 use std::collections::BTreeMap;
 use std::fmt::Debug;
+use std::ops::{Add, Mul, Sub};
 
 use itertools::Itertools;
 use num::bigint::{RandBigInt, ToBigUint};
@@ -351,6 +352,36 @@ where
     }
 }
 
+impl<K> Die<K>
+where
+    K: Clone + Ord,
+{
+    pub fn max(&self, rhs: &Self) -> Self {
+        self.combine_with(rhs, |x, y| x.max(y).clone())
+    }
+
+    pub fn min(&self, rhs: &Self) -> Self {
+        self.combine_with(rhs, |x, y| x.min(y).clone())
+    }
+
+    pub fn max_of(&self, count: usize) -> Self {
+        self.repeat(count, |x, y| x.max(y).clone())
+    }
+
+    pub fn min_of(&self, count: usize) -> Self {
+        self.repeat(count, |x, y| x.min(y).clone())
+    }
+}
+
+impl<K> Die<K>
+where
+    K: Copy + Ord + Add<K, Output = K>,
+{
+    pub fn sum_of(&self, count: usize) -> Self {
+        self.repeat(count, |x, y| *x + *y)
+    }
+}
+
 impl<'a, G> Approximator<'a, G>
 where
     G: RngCore,
@@ -403,5 +434,159 @@ where
                 .map(|x| x.to_biguint().unwrap())
                 .collect(),
         }
+    }
+}
+
+impl<L, R> Add<Die<R>> for Die<L>
+where
+    L: Copy + Add<R>,
+    R: Copy,
+    <L as Add<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Add<R>>::Output>;
+
+    fn add(self, rhs: Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x + *y)
+    }
+}
+
+impl<L, R> Add<&Die<R>> for Die<L>
+where
+    L: Copy + Add<R>,
+    R: Copy,
+    <L as Add<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Add<R>>::Output>;
+
+    fn add(self, rhs: &Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x + *y)
+    }
+}
+
+impl<L, R> Add<Die<R>> for &Die<L>
+where
+    L: Copy + Add<R>,
+    R: Copy,
+    <L as Add<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Add<R>>::Output>;
+
+    fn add(self, rhs: Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x + *y)
+    }
+}
+
+impl<L, R> Add<&Die<R>> for &Die<L>
+where
+    L: Copy + Add<R>,
+    R: Copy,
+    <L as Add<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Add<R>>::Output>;
+
+    fn add(self, rhs: &Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x + *y)
+    }
+}
+
+impl<T, K> Add<T> for Die<K>
+where
+    K: Copy + Add<T>,
+    T: Copy + Ord,
+    <K as Add<T>>::Output: Copy,
+{
+    type Output = Die<<K as Add<T>>::Output>;
+
+    fn add(self, rhs: T) -> Self::Output {
+        self.biect_map(|x| x + rhs)
+    }
+}
+
+impl<L, R> Sub<Die<R>> for Die<L>
+where
+    L: Copy + Sub<R>,
+    R: Copy,
+    <L as Sub<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Sub<R>>::Output>;
+
+    fn sub(self, rhs: Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x - *y)
+    }
+}
+
+impl<L, R> Sub<&Die<R>> for Die<L>
+where
+    L: Copy + Sub<R>,
+    R: Copy,
+    <L as Sub<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Sub<R>>::Output>;
+
+    fn sub(self, rhs: &Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x - *y)
+    }
+}
+
+impl<L, R> Sub<Die<R>> for &Die<L>
+where
+    L: Copy + Sub<R>,
+    R: Copy,
+    <L as Sub<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Sub<R>>::Output>;
+
+    fn sub(self, rhs: Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x - *y)
+    }
+}
+
+impl<L, R> Sub<&Die<R>> for &Die<L>
+where
+    L: Copy + Sub<R>,
+    R: Copy,
+    <L as Sub<R>>::Output: Ord + Copy,
+{
+    type Output = Die<<L as Sub<R>>::Output>;
+
+    fn sub(self, rhs: &Die<R>) -> Self::Output {
+        self.combine_with(rhs, |x, y| *x - *y)
+    }
+}
+
+impl<T, K> Sub<T> for Die<K>
+where
+    K: Copy + Sub<T>,
+    T: Copy + Ord,
+    <K as Sub<T>>::Output: Copy,
+{
+    type Output = Die<<K as Sub<T>>::Output>;
+
+    fn sub(self, rhs: T) -> Self::Output {
+        self.biect_map(|x| x - rhs)
+    }
+}
+
+impl<T, K> Mul<T> for Die<K>
+where
+    K: Copy + Ord + Add<K, Output = K>,
+    T: Into<usize>,
+{
+    type Output = Die<K>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        self.repeat(rhs.into(), |x, y| *x + *y)
+    }
+}
+
+impl<T, K> Mul<T> for &Die<K>
+where
+    K: Copy + Ord + Add<K, Output = K>,
+    T: Into<usize>,
+{
+    type Output = Die<K>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        self.repeat(rhs.into(), |x, y| *x + *y)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 mod defs;
 mod die;
-mod ops;
 
 pub use defs::*;
 pub use die::{Approximator, Die, Error, Result};
